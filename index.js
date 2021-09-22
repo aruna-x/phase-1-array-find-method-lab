b261c88a24f6f5cdcc8a6b618ef567f9824f49af
Aruna <aruna.evan15@gmail.com>
2021-09-22 10:52:21 -0700

    commit tree b4ccf422f04a50f9c374792656de9d64057786c6

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+const superbowlWin = (record) => {
+    const wFound = record.find(isW);
+    if (wFound) {
+        return wFound['year'];
+    } else {
+        return undefined;
+    }
+}
+
+function isW(el) {
+    if(el['result'] === 'W') {
+        return true;
+    } else {
+        return false;
+    }
+}
